a52f1aac25dd6aa9fac45412a2ca8180eaae88b5
natesurf <natesurf@yahoo.com>
2018-07-26 14:20:18 -0700

    added TodoList

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var dummyData = [
+  "Wash the dishes",
+  "Take out the trash",
+  "Lorem ipsum delorum",
+  "Finish some really difficult and challenging task that requires a lot of characters in a string.",
+  "idk",
+  "learn react",
+  "make a todo list manager"
+];
 
-ReactDOM.render((<p> asdf </p>), document.getElementById('root'));
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li> {this.props.description} </li>
+    )
+  }
+}
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul> {dummyData.map((d, i) => <Todo key={i} description={d} />)}</ul>
+    )
+  }
+}
+ReactDOM.render(<TodoList />, document.getElementById('root'));
